5474b9652b039edf807ac3f3cf2072f1ad9894ce
Iman <iman.osama2000@gmail.com>
2023-08-01 12:23:54 +0300

    generated models

---
+++ Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scenario_13.Models;
+
+public partial class Comment
+{
+    public long Id { get; set; }
+
+    public long BlogId { get; set; }
+
+    public long UserId { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public virtual Blog Blog { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
---
+++ Models/PostgresContext.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Scenario_13.Models;
+
+public partial class PostgresContext : DbContext
+{
+    public PostgresContext()
+    {
+    }
+
+    public PostgresContext(DbContextOptions<PostgresContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Blog> Blogs { get; set; }
+
+    public virtual DbSet<Comment> Comments { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("host=localhost;port=5432;Database=postgres;username=postgres;password=postgres;sslmode=prefer");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Blog>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("blog_pkey");
+
+            entity.ToTable("blog");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.AuthorId).HasColumnName("authorId");
+            entity.Property(e => e.CreationDate)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnName("creationDate");
+            entity.Property(e => e.Text).HasColumnName("text");
+            entity.Property(e => e.Title)
+                .HasMaxLength(75)
+                .HasColumnName("title");
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Blogs)
+                .HasForeignKey(d => d.AuthorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_blog_user");
+        });
+
+        modelBuilder.Entity<Comment>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("comment_pkey");
+
+            entity.ToTable("comment");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.BlogId).HasColumnName("blogId");
+            entity.Property(e => e.Date)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnName("date");
+            entity.Property(e => e.UserId).HasColumnName("userId");
+
+            entity.HasOne(d => d.Blog).WithMany(p => p.Comments)
+                .HasForeignKey(d => d.BlogId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_comment_blog");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Comments)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_comment_user");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("user_pkey");
+
+            entity.ToTable("user");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .HasColumnName("email");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .HasColumnName("firstName");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .HasColumnName("lastName");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
